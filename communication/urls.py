d99adbac4b8f2eda1af429bf2859988b16c0c95d
jackiecherotich <jackie.cherotich@gmail.com>
2025-12-15 14:11:06 +0000

    Delete communication directory

--- communication/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = "communication"
-
-urlpatterns = [
-    path("consultation/<uuid:consultation_id>/", views.consultation_room, name="consultation_room"),
-]
